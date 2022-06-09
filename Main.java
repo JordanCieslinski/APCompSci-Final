819a9488dee60063647b73dffde1c5f31b549201
Rylan Meilutis <44624900+Rylan-Meilutis@users.noreply.github.com>
2022-06-08 22:48:10 -0400

    formatted

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static player_2 p2;
+
     public static void main(String[] args) {
         p2 = new player_2();
         new StartScreen();
-        
-        
+
+
     }
 
 }
